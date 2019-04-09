b3b88e78e88dc397c9124c846a06f6cfcb83a126
kowshik gunda <kgunda@uco.edu>
2019-04-09 15:14:43 -0500

    add sign up
    we did singup and forgot password

--- tutor-monitor/src/app/app-routing.module.ts
+++ tutor-monitor/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { AuthGuard } from "./shared/gaurd/auth.gaurd";
+import { SecureInnerPagesGuard } from "./shared/gaurd/secure-inner-pages.gaurd";
 
 const routes: Routes = [
-  { path: 'users', component: UsersComponent},
-  { path: '', redirectTo: '/users', pathMatch: 'full'}
+  { path: 'users', component: UsersComponent },
+  { path: 'calendar', component: CalendarComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'register-user', component: SignUpComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'verify-email-address', component: VerifyEmailComponent, canActivate: [SecureInnerPagesGuard] }
+
 ];
 
 
--- tutor-monitor/src/app/app.component.ts
+++ tutor-monitor/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth'
 import { Observable } from 'rxjs';
 
 @Component({
@@ -7,10 +8,19 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'admin';
   items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
+  constructor(db: AngularFirestore, private afAuth: AngularFireAuth) {
     this.items = db.collection('items').valueChanges();
   }
+
+  ngOnInit() {
+    this.afAuth.authState
+      .subscribe(user => {
+        console.log(user);
+
+      })
+  }
+
 }
